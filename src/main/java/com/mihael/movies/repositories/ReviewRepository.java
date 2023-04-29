4d42e25a2322476cbff3931a6892d307d90aa421
Miiso9 <mihaelmiso555@gmail.com>
2023-04-29 16:13:26 +0200

    Funckionalno

---
+++ src/main/java/com/mihael/movies/repositories/ReviewRepository.java
@@ -0,0 +1,10 @@
+package com.mihael.movies.repositories;
+
+import com.mihael.movies.models.Review;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
